63a67ea55e4b0074ca696a0bfc47986b1d1bb0a0
Nicolas Ries <nries1@gmail.com>
2019-10-14 21:47:17 -0400

    init commit

--- src/callbacker.js
+++ src/callbacker.js
@@ -1,5 +1,27 @@
-const syncCallbacker = null;
+const syncCallbacker = (res, rej) => {
+  if (
+    typeof res !== 'function' ||
+    typeof rej !== 'function' ||
+    arguments.length < 2
+  )
+    throw new Error('You must provide two function parameters');
+  return rej(res());
+};
 
-const asyncCallbacker = null;
+const asyncCallbacker = (res, rej) => {
+  if (
+    typeof res !== 'function' ||
+    typeof rej !== 'function' ||
+    arguments.length < 2
+  )
+    throw new Error('You must provide two function parameters');
+  //   for (let i = 2; i < arguments.length; i++) {
+  //     console.log(typeof res(arguments[i], rej));
+  //   }
+  for (let i = 2; i < arguments.length; i++) {
+    let result = res(arguments[i], rej);
+    if (!result) return;
+  }
+};
 
 module.exports = { syncCallbacker, asyncCallbacker };
